249304efa7ba27de24cecc3a618e0d849d4f2b99
Aubrey Yates <aubreyyates999@hotmail.com>
2023-10-12 08:33:57 -0500

    Add demonstration of using the strategy pattern in the Program.cs file.

--- StrategyPattern/Program.cs
+++ StrategyPattern/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using StrategyPattern;
+using StrategyPattern.RouteAlgorithms;
+
+// Instantiate a BusRouteCalculator that uses RouteAlgorithm1 to calculate bus routes.
+BusRouteCalculator busRouteCalculator = new BusRouteCalculator(new RouteAlgorithm1());
+
+// Prints "The bus stop, The mall - route 1".
+Console.WriteLine(busRouteCalculator.CalculateBusRoute("The bus stop", "The mall"));
+
+// Change the route algorithm to use in the BusRouteCalculator. This will change the algorithm used without needing to
+// change the BusRouteCalculator.
+busRouteCalculator = new BusRouteCalculator(new RouteAlgorithm2());
+
+// Prints "The bus stop, The mall - route 2".
+Console.WriteLine(busRouteCalculator.CalculateBusRoute("The bus stop", "The mall"));
